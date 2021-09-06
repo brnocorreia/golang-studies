f40e4950f56aebe34bf08c226ef8e543e9319288
Bruno Correia <dev.brunocorreia@gmail.com>
2021-09-06 16:29:26 -0300

    modularizando

--- hello/hello.go
+++ hello/hello.go
@@ -1,19 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	nome := "Douglas"
-	versao := 1.1
-	fmt.Println("Hello, Mr.", nome)
-	fmt.Println("Este programa está na versão", versao)
+	programIntroduction()
 
+	commandMenu()
+
+	command := commandSelector()
+
+	switch command {
+	case 1:
+		initMonitoring()
+	case 2:
+		fmt.Println("Exibindo Logs")
+	case 3:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Comando indisponível")
+		os.Exit(-1)
+	}
+}
+
+func programIntroduction() {
+	fmt.Println("Digite o seu nome:")
+	var nome string
+	fmt.Scan(&nome)
+	versao := 1.1
+	fmt.Println("Olá, Sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func commandSelector() int {
+	var command int
+	fmt.Scan(&command)
+	return command
+}
+
+func commandMenu() {
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("3 - Sair do Programa")
+}
 
-	var comando int
-	fmt.Scan(&comando)
-
-	fmt.Println("O valor da variável comando é", comando)
+func initMonitoring() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br"
+	resp, _ := http.Get(site)
+	fmt.Println(resp)
 }
