28fed92eb6ec6cd04461524056f7704767408966
Bruno Correia <dev.brunocorreia@gmail.com>
2021-09-06 15:27:07 -0300

    scan e var

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World")
+	nome := "Douglas"
+	versao := 1.1
+	fmt.Println("Hello, Mr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("3 - Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O valor da variável comando é", comando)
 }
